59977d68a125b72c2806ada1619a496c86f836be
mohabrd <bezrhoudimohamed@gmail.com>
2023-01-06 12:22:34 +0100

    structure il était temps

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+// import { Personnage, perso2 } from "./module/all.js";
+import * as CLASS from "./module/all.js";
+// import {test} from "./module/instances.js"
+
+//EX:
+let perso1 = new CLASS.Personnage("Bergmans","Johan");
+
+console.log(perso1,CLASS.perso2);
---
+++ public/js/module/all.js
@@ -0,0 +1,10 @@
+//EX:
+
+export class Personnage{
+    constructor(nom,prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
+
+export let perso2 = new Personnage("Cactus","Kbib");
